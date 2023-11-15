0c443782644fd7edd6cdba2699a119942ae70a4e
Diversable <dan.mantei@outlook.com>
2023-11-15 15:31:38 -0800

    built leptos CSR

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <h1>"Hello from Leptos (and Spin?)"</h1>
+
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+    }
+}
+
+fn main() {
+    mount_to_body(|| view! { <App /> })
+}
