b74d90ddc93618efe1286a7cbd588c01e6adfb4d
Diversable <dan.mantei@outlook.com>
2023-12-18 22:33:10 -0800

    update example page

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
 use leptos::*;
 
 #[component]
-fn App() -> impl IntoView {
+fn App(increment: i32) -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
-        <h1>"Hello from Leptos and Spin"</h1>
+    <div class="container">
 
-        <p>
-            "For ease of development, run `trunk watch` in one terminal while also running `spin watch` in another terminal -- on changes, the only thing you need to do is reload the browser window: Leptos and Spin will take care of the rest!"
-        </p>
+            <picture>
+                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
+                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
+            </picture>
+
+        <h1>"Welcome to Leptos"</h1>
+        <h2><i>"On Spin - Serverless WASI"</i></h2>
 
         <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1);
+            on:click= move |_| {
+                set_count(count() + increment)
             }
         >
             "Click me: "
-            {move || count.get()}
+            {count}
         </button>
+
+
+    </div>
     }
 }
 
 fn main() {
-    mount_to_body(|| view! { <App /> })
+    mount_to_body(|| {
+        view! {
+            <App increment=5 />
+        }
+    })
 }
